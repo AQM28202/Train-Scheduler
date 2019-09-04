0a89f38f2c09a95412c8b76a0f967274697209e6
Anthony McPherson <aqm28202@gmail.com>
2019-09-04 16:27:33 -0400

    <using bootstrap to build layout and pasting firebase config into .js file>

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var firebaseConfig = {
+        apiKey: "AIzaSyDYi50vWY4E3Oq0UzZ_4cidt00x_AekiHk",
+        authDomain: "new-project-fcbd5.firebaseapp.com",
+        databaseURL: "https://new-project-fcbd5.firebaseio.com",
+        projectId: "new-project-fcbd5",
+        storageBucket: "",
+        messagingSenderId: "532445689870",
+        appId: "1:532445689870:web:054651904477fa59"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
+   
