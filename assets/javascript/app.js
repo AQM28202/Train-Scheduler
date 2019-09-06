2263d9b65df9f3a4b6ee206a8020296567a71c9b
Anthony McPherson <aqm28202@gmail.com>
2019-09-06 18:35:21 -0400

    creating other variables for onclick event

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,34 @@
-$(document).ready(function(){
-    var firebaseConfig = {
-        apiKey: "AIzaSyDYi50vWY4E3Oq0UzZ_4cidt00x_AekiHk",
-        authDomain: "new-project-fcbd5.firebaseapp.com",
-        databaseURL: "https://new-project-fcbd5.firebaseio.com",
-        projectId: "new-project-fcbd5",
-        storageBucket: "",
-        messagingSenderId: "532445689870",
-        appId: "1:532445689870:web:054651904477fa59"
-      };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
+$(document).ready(function () {
+  var firebaseConfig = {
+    apiKey: "AIzaSyDYi50vWY4E3Oq0UzZ_4cidt00x_AekiHk",
+    authDomain: "new-project-fcbd5.firebaseapp.com",
+    databaseURL: "https://new-project-fcbd5.firebaseio.com",
+    projectId: "new-project-fcbd5",
+    storageBucket: "",
+    messagingSenderId: "532445689870",
+    appId: "1:532445689870:web:054651904477fa59"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-      var database = firebase.database();
+  var database = firebase.database();
 
-      var name; 
-      $("#add-train").on("click", function() {
-        event.preventDefault();
+  // Creating variables for the onClick event
+  var name;
+  var destination;
+  var firstTrain;
+  var frequency = 0;
 
-         // Storing and retreiving new train data
-         name = $("#train-name").val().trim();
+  $("#add-train").on("click", function () {
+    event.preventDefault();
+
+    // Storing and retreiving new train data
+    name = $("#train-name").val().trim();
 
     // Pushing to database
-      database.ref().push({
-      name: name,      
-   
+    database.ref().push({
+      name: name,
+
     });
   });
 });
-  
